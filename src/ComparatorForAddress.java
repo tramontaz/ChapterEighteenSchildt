d5c3c62762e17157324f1568569355574f0b4b3d
dragon <v.chemodurov@yandex.ru>
2017-09-08 23:36:13 +0300

    +add ComparatorForAddress demo

---
+++ src/ComparatorForAddress.java
@@ -0,0 +1,46 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class ComparatorForAddress {
+
+    static class MyComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String firstStr, String secondStr) {
+            int lastInxOfFirstStr,lastInxOfSecondStr, compareSubStrings;
+
+            lastInxOfFirstStr = firstStr.lastIndexOf(' ');
+            lastInxOfSecondStr = secondStr.lastIndexOf(' ');
+            compareSubStrings = firstStr.substring(lastInxOfFirstStr).compareTo(secondStr.substring(lastInxOfSecondStr));
+            if (compareSubStrings == 0) return firstStr.compareTo(secondStr);
+            else return compareSubStrings;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // create tree map
+        TreeMap<String, Double> tm = new TreeMap<>(new MyComparator());
+
+        //input elements into tree map
+        tm.put("John Dow", 3434.34);
+        tm.put("Tom Smith", 123.22);
+        tm.put("Jane Baker", 1378.00);
+        tm.put("Tod Hall", 99.22);
+        tm.put("Ralph Smith", -19.08);
+
+        //get a set from tree map
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+
+        //output set
+        for(Map.Entry<String, Double> mapEntry : set){
+            System.out.print(mapEntry.getKey() + ": " + mapEntry.getValue() + "\n");
+        }
+
+        //popup John's balance
+        tm.put("John Dow", tm.get("John Dow") + 1000);
+        System.out.println("New balance on John's account: " +  tm.get("John Dow"));
+    }
+}
